f1ab33b39aca9065f82f346b900cba0b10644f75
Administrador <pedropablo2337@gmail.com>
2021-06-19 11:08:08 -0400

    cambios nuevos en el listado

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    api_host: 'http://localhost:4200'
+    api_host: 'https://pparce.github.io'
 };
 /*
  * For easier debugging in development mode, you can import the following file
